2eadaac44dc4ed2e00cf99ce12acaa87d498575a
Sai Nadkarni <sai2010nadkarni@gmail.com>
2020-02-22 18:50:17 +0530

    remove lokijs

--- src/di/core-repository.ts
+++ src/di/core-repository.ts
@@ -6,22 +6,11 @@ import IRepository from '../core/repository/definition';
 // Interface imports end
 
 // Entity imports begin
-import LokiRepo from '../core/repository/loki';
 import MongoRepo from '../core/repository/mongo';
 // Entity imports end
- 
+
 const container = new Container();
 
-switch(process.env['REPOSITORY.DEFAULT']) {
-  case 'MONGO':
-    container.bind<IRepository>(TYPES.IRepository).to(MongoRepo).inSingletonScope();
-    break;
-  case 'LOKI':
-    container.bind<IRepository>(TYPES.IRepository).to(LokiRepo).inSingletonScope();
-    break;
-  default:
-    container.bind<IRepository>(TYPES.IRepository).to(LokiRepo).inSingletonScope();
-    break;
-}
+container.bind<IRepository>(TYPES.IRepository).to(MongoRepo).inSingletonScope();
 
 export default container;
